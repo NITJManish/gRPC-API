212c0eccc04b6e08241fb493a4ceb6573c785160
neera <kumarmanish150799@gmail.com>
2024-07-09 10:08:35 +0530

    gRPC API

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -4,7 +4,6 @@ const connectDatabase=()=>{
     let DB_URL="";
 
     if(process.env.NODE_ENV==="DEVELOPMENT") DB_URL=process.env.DB_URL;
-    // if(process.env.NODE_ENV==="PRODUCTION") DB_URL=process.env.DB_URL;
 
     mongoose.connect(DB_URL).then((con)=>{
         console.log(`mongodb database connected with HOST : ${con?.connection?.host}`);
